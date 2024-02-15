40e6aa94a0fc7ffbb50b4de3fae3d3991b5e1a66
fbansept <bansept.franck@gmail.com>
2024-02-15 15:55:02 +0100

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import * as Notifications from "expo-notifications";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs(["new NativeEventEmitter"]);
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false, //dépend du téléphone et de sa configuration
+    shouldSetBadge: false,
+  }),
+});
+
+Notifications.cancelAllScheduledNotificationsAsync();
+Notifications.scheduleNotificationAsync({
+  content: {
+    title: "Votre récompense est disponible",
+    body: "N'oubliez pas de venir collecter votre récompense !",
+  },
+  trigger: {
+    seconds: 10,
+    //seconds: 60, //1 minute
+    // seconds: 60 * 60, //1 hour
+    // seconds: 60 * 60 * 24, //1 day
+    //repeats: true,
+  },
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Vous devriez voir une notification</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
